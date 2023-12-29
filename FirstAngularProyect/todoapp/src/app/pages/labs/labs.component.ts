ad26dc9805485273d950f77c9f88153606d8d1c5
= <=>
2023-12-29 18:36:59 -0300

    clase 10

--- FirstAngularProyect/todoapp/src/app/pages/labs/labs.component.ts
+++ FirstAngularProyect/todoapp/src/app/pages/labs/labs.component.ts
@@ -1,19 +1,43 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
 export class LabsComponent {
   welcome = 'Welcome';
-  tasks = [
+  tasks = signal([
     'Install Angular CLI',
     'Create Proyect',
     'Create Components'
-  ];
-  name = 'René';
-  age = 33
+  ]);
+  name = signal('René');
+  age = 33;
+  disabled = true;
+  img = 'https://w3schools.com/howto/img_avatar.png';
+
+  person = {
+    name: 'René',
+    age: 33,
+    avatar: 'https://w3schools.com/howto/img_avatar.png'
+  }
+
+  clickhandler() {
+    alert('Hola')
+  }
+
+  changehandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue);
+  }
+
+  keydownhandler(event: KeyboardEvent) {
+const input = event.target as HTMLInputElement;
+console.log(input.value);
+  }
 }
